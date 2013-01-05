fcb9dcf14d67b61747fe9dc71f49ff6a4574f882
Alexey <leopard.not.a@gmail.com>
2013-01-05 18:51:32 +0200

    own cookbook

---
+++ site-cookbooks/tomatoes/recipes/default.rb
@@ -0,0 +1,22 @@
+package "git"
+
+%w(public logs).each do |dir|
+  directory "#{node.app.web_dir}/#{dir}" do
+    owner node.user.name
+    mode "0755"
+    recursive true
+  end
+end
+
+template "#{node.nginx.dir}/sites-available/#{node.app.name}.conf" do
+  source "nginx.conf.erb"
+  mode "0644"
+end
+
+nginx_site "#{node.app.name}.conf"
+
+cookbook_file "#{node.app.web_dir}/public/index.html" do
+  source "index.html"
+  mode 0755
+  owner node.user.name
+end
