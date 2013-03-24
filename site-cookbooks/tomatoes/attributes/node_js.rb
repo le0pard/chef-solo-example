bcc057d001e8cdfe33a7120f13a11a239a8dce71
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-24 06:11:10 +0200

    update repo versions

--- site-cookbooks/tomatoes/attributes/node_js.rb
+++ site-cookbooks/tomatoes/attributes/node_js.rb
@@ -1,4 +1,4 @@
-default['nodejs']['version'] = '0.8.6'
-default['nodejs']['checksum'] = 'dbd42800e69644beff5c2cf11a9d4cf6dfbd644a9a36ffdd5e8c6b8db9240854'
+default['nodejs']['version'] = '0.10.1'
+default['nodejs']['checksum'] = 'd4d9ef3b70452112246ea2043ef75943611e3537'
 default['nodejs']['dir'] = '/usr/local'
 default['nodejs']['src_url'] = "http://nodejs.org/dist"
